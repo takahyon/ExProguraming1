e2226b0cd715e398b293b217b0ddbe9c12ed1608
Shouta Harikae <mail@havicon.net>
2017-04-10 16:41:08 +0900

    First comitt

---
+++ src/en01/Ex01.java
@@ -0,0 +1,14 @@
+package en01;
+
+/**
+ * Created by shouta on 2017/04/10.
+ */
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println("Tokyo");
+        System.out.println("University");
+        System.out.println("of");
+        System.out.println("Technology");
+
+    }
+}
